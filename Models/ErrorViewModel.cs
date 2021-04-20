83766ab10f4eacc0b4dd18491125cde7dbba1cd5
DESKTOP-DA1B8MP\Beelzebub <sanchesik@bk.ru>
2021-04-20 11:54:22 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HumanResourcesDepartment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
